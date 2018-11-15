8657ae4011e1bc24d67f8cc5a667705339953601
Jane <jane.edwards.mcnear@gmail.com>
2018-11-15 09:24:52 -0800

    completed drawLetters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,47 @@
 const Adagrams = {
+
   drawLetters() {
-    // Implement this method for wave 1
-  },
+    const letterPool = [
+      'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+      'B', 'B',
+      'C', 'C',
+      'D', 'D', 'D', 'D',
+      'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+      'F', 'F',
+      'G', 'G', 'G',
+      'H', 'H',
+      'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+      'J',
+      'K',
+      'L', 'L', 'L', 'L',
+      'M', 'M',
+      'N', 'N', 'N', 'N', 'N', 'N',
+      'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+      'P', 'P',
+      'Q',
+      'R', 'R', 'R', 'R', 'R', 'R',
+      'S', 'S', 'S', 'S',
+      'T', 'T', 'T', 'T', 'T', 'T',
+      'U', 'U', 'U', 'U',
+      'V', 'V',
+      'W', 'W',
+      'X',
+      'Y', 'Y',
+      'Z'
+    ];
+
+    let pool = []
+    let indexes = []
+    while (pool.length != 10) {
+      let index = Math.floor(Math.random() * 99);
+      if (!(indexes.includes(index))){ // if index is not already in the index array
+        pool.push(letterPool[index]);
+        indexes.push(index);
+      }
+    }
+    return pool;
+  }
+
 };
 
 // Do not remove this line or your tests will break!
